08628de66b5fc940d3f814419d966c957ba02cbc
foultrip <davidvasquezmahecha@gmail.com>
2024-05-08 10:53:40 -0500

    pep correction 01

--- models/amenity.py
+++ models/amenity.py
@@ -10,14 +10,26 @@ Base = declarative_base()
 place_amenity = Table(
     "place_amenity",
     Base.metadata,
-    Column("place_id", String(60), ForeignKey("places.id"), primary_key=True),
-    Column("amenity_id", String(60), ForeignKey("amenities.id"), primary_key=True),
+    Column(
+        "place_id",
+        String(60),
+        ForeignKey("places.id"),
+        primary_key=True),
+    Column("amenity_id",
+           String(60),
+           ForeignKey("amenities.id"),
+           primary_key=True),
 )
 
 
 class Amenity(BaseModel, Base):
     __tablename__ = "amenities"
-    name = Column(String(128), nullable=True)
+    name = Column(
+        String(128),
+        nullable=True
+    )
     place_amenities = relationship(
-        "Place", secondary=place_amenity, backref="amenities"
+        "Place",
+        secondary=place_amenity,
+        backref="amenities"
     )
--- models/place.py
+++ models/place.py
@@ -12,8 +12,17 @@ Base = declarative_base()
 place_amenity = Table(
     "place_amenity",
     Base.metadata,
-    Column("place_id", String(60), ForeignKey("places.id"), primary_key=True),
-    Column("amenity_id", String(60), ForeignKey("amenities.id"), primary_key=True),
+    Column(
+        "place_id",
+        String(60),
+        ForeignKey("places.id"),
+        primary_key=True),
+    Column(
+        "amenity_id",
+        String(60),
+        ForeignKey("amenities.id"),
+        primary_key=True
+    ),
 )
 
 
@@ -21,18 +30,60 @@ class Place(BaseModel, Base):
     """A place to stay"""
 
     __tablename__ = "places"
-    city_id = Column(String(60), ForeignKey("cities.id"), nullable=True)
-    user_id = Column(String(60), ForeignKey("users.id"), nullable=True)
-    name = Column(String(128), nullable=True)
-    description = Column(String(1024), nullable=True)
-    number_rooms = Column(Integer, default=0, nullable=True)
-    number_bathrooms = Column(Integer, default=0, nullable=True)
-    max_guest = Column(Integer, default=0, nullable=True)
-    price_by_night = Column(Integer, default=0, nullable=True)
-    latitude = Column(Float, default=0, nullable=True)
-    longitude = Column(Float, default=0, nullable=True)
+    city_id = Column(
+        String(60),
+        ForeignKey("cities.id"),
+        nullable=True
+    )
+    user_id = Column(
+        String(60),
+        ForeignKey("users.id"),
+        nullable=True
+    )
+    name = Column(
+        String(128),
+        nullable=True
+    )
+    description = Column(
+        String(1024),
+        nullable=True
+    )
+    number_rooms = Column(
+        Integer,
+        default=0,
+        nullable=True
+    )
+    number_bathrooms = Column(
+        Integer,
+        default=0,
+        nullable=True
+    )
+    max_guest = Column(
+        Integer,
+        default=0,
+        nullable=True
+    )
+    price_by_night = Column(
+        Integer,
+        default=0,
+        nullable=True
+    )
+    latitude = Column(
+        Float,
+        default=0,
+        nullable=True
+    )
+    longitude = Column(
+        Float,
+        default=0,
+        nullable=True
+    )
     amenity_ids = []
-    amenities = relationship("Amenity", secondary=place_amenity, viewonly=False)
+    amenities = relationship(
+        "Amenity",
+        secondary=place_amenity,
+        viewonly=False
+    )
 
     @property
     def amenities(self):
